513533314c11937f90f0cdbb0774867cf3286b5d
Administrator <Administrator@admin-PC>
2018-07-23 19:13:26 +0800

    外星人创建第二部分（第二次提交）

---
+++ alienInvade/alien.py
@@ -0,0 +1,77 @@
+#coding=UTF-8
+'''
+Created on 2018年7月23日
+
+@author: Administrator
+
+创建第一个外星人
+'''
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    """表示单个外星人的类"""
+    
+    def __init__(self,ai_settings,screen):
+        """初始化外星人 并设置其位置"""
+        super(Alien,self).__init__()
+        self.screen = screen
+        self.ai_settings = ai_settings;
+        
+        #加载外星人图像，并设置其rect属性
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+        
+        #每个外星人最初都在屏幕左上角附近
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        
+        #存储外星人的准确坐标
+        self.x = float(self.rect.x)
+        
+    def update(self):
+        """向右或者向左移动外星人 fleet_direction可能是负数"""
+        self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction)
+        self.rect.x = self.x
+        
+    def blitme(self):
+        """在指定位置绘制外星人"""
+        self.screen.blit(self.image,self.rect)
+        
+    def check_edges(self):
+        """如果外星人位于屏幕边缘 就返回True"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
+        
+        
+        
+      
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ alienInvade/game_stats.py
@@ -0,0 +1,19 @@
+#coding=UTF-8
+'''
+Created on 2018年7月23日
+
+@author: Administrator
+'''
+class GameStats():
+    """跟踪游戏的统计信息"""
+    
+    def __init__(self,ai_sittings):
+        """初始化统计信息"""
+        self.ai_settings = ai_sittings
+        self.reset_status()
+        #游戏刚启动时处于活动状态
+        self.game_active = True
+        
+    def reset_status(self):
+        """初始化在游戏运行期间可能变化的统计信息"""
+        self.ships_left = self.ai_settings.ship_limit#飞船数量
